5e1a11e4726e80fc4e1c09dcaa667e6869db9c60
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-09-16 00:28:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+const body = document.getElementsByTag("body")[0];
+const str = document.createElement('p');
+str.textContent = "DOM load success";
+body.appendChild('str');
