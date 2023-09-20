46c9c203089706d27056b222ec9aab1c7a6a5b73
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-09-21 02:08:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 document.addEventListener("DOMContentLoaded", function() {
     const body = document.body;
+	body.innerHTML = "";
     const str = document.createElement('p');
     str.textContent = "DOM load success";
     body.appendChild(str);
